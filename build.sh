74ab3433e52327c021c26132a206c117c09959bc
gerblesh <101901964+gerblesh@users.noreply.github.com>
2024-12-26 15:06:58 -0800

    add tailscale

--- build.sh
+++ build.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-dnf install -y cockpit cockpit-storaged cockpit-podman cockpit-navigator cockpit-networkmanager cockpit-files cockpit-composer
+dnf install -y cockpit cockpit-storaged cockpit-podman cockpit-navigator cockpit-networkmanager cockpit-files cockpit-composer tailscale
 systemctl enable podman.socket
 systemctl enable podman-auto-update.timer
 systemctl enable cockpit
